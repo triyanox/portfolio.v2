e974d5e7df85cb844f3236ef687c6aff86cda39d
Mohamed Achaq <motriyanox@gmail.com>
2022-06-14 22:57:34 +0100

    adding accessible names

--- components/ui/LikeButton.tsx
+++ components/ui/LikeButton.tsx
@@ -1,25 +1,26 @@
-import { TiHeartFullOutline } from "react-icons/ti";
-import { useSWRConfig } from "swr";
+import { TiHeartFullOutline } from 'react-icons/ti'
+import { useSWRConfig } from 'swr'
 
 type LikeProps = {
-  slug: string;
-};
+  slug: string
+}
 
 const Like = ({ slug }: LikeProps) => {
-  const { mutate } = useSWRConfig();
+  const { mutate } = useSWRConfig()
   const handleLike = async () => {
     await fetch(`/api/posts/likes/${slug}`, {
-      method: "POST",
-    });
-    mutate("/api/posts/likes/" + slug);
-  };
+      method: 'POST',
+    })
+    mutate('/api/posts/likes/' + slug)
+  }
   return (
     <button
+      aria-label="Like"
       onClick={handleLike}
-      className="bg-zinc-900  fixed bottom-0 shadow-lg text-white dark:text-black right-0 z-50 mr-8 md:mr-20 mb-8 dark:bg-zinc-50 active:text-red-500 active:dark:text-red-600 rounded-full px-3 py-3 ring-0 active:ring-4 ring-zinc-200 dark:ring-zinc-800 hover:ring-4 hover:scale-105 transition-all duration-300 active:scale-95 "
+      className="fixed  bottom-0 right-0 z-50 mr-8 mb-8 rounded-full bg-zinc-900 px-3 py-3 text-white shadow-lg ring-0 ring-zinc-200 transition-all duration-300 hover:scale-105 hover:ring-4 active:scale-95 active:text-red-500 active:ring-4 dark:bg-zinc-50 dark:text-black dark:ring-zinc-800 active:dark:text-red-600 md:mr-20 "
     >
       <TiHeartFullOutline className="text-3xl " />
     </button>
-  );
-};
-export default Like;
+  )
+}
+export default Like
