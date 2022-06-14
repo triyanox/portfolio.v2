58a4112b1a504f085f1110c0684fc62436ad1b4a
Mohamed Achaq <motriyanox@gmail.com>
2022-06-14 22:57:30 +0100

    adding accessible names

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -4,13 +4,13 @@ import {
   SiGithub,
   SiTwitter,
   SiYoutube,
-} from "react-icons/si/index.js";
-import Link from "next/link";
-import { IconButton } from "./Buttons";
+} from 'react-icons/si/index.js'
+import Link from 'next/link'
+import { IconButton } from './Buttons'
 
 const Footer = () => {
   return (
-    <footer className="px-6 w-full md:px-24 flex flex-col pb-16 items-center justify-center  gap-y-4 pt-36 md:pt-48 lg:flex-row">
+    <footer className="flex w-full flex-col items-center justify-center gap-y-4 px-6 pb-16  pt-36 md:px-24 md:pt-48 lg:flex-row">
       <div className="flex w-full flex-col items-center ">
         <div className="flex w-full flex-col items-start justify-center gap-2 text-2xl ">
           <h2 className=" text-2xl font-semibold text-gray-800 dark:text-gray-200">
@@ -22,18 +22,18 @@ const Footer = () => {
           <div className=" mt-4 flex w-full flex-row items-center gap-4 ">
             <Link href="https://github.com/triyanox" passHref>
               <a aria-label="Github" target="_blank">
-                <IconButton icon={<SiGithub />} />
+                <IconButton aria="Github" icon={<SiGithub />} />
               </a>
             </Link>
             <Link href="https://twitter.com/ac__haq" passHref>
               <a aria-label="Twitter" target="_blank">
-                <IconButton icon={<SiTwitter />} />
+                <IconButton aria="Twitter" icon={<SiTwitter />} />
               </a>
             </Link>
           </div>
         </div>
       </div>
     </footer>
-  );
-};
-export default Footer;
+  )
+}
+export default Footer
