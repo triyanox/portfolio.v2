a7cd2aa756308f2048690c10201ce63f4603a44c
Mohamed Achaq <motriyanox@gmail.com>
2022-07-21 16:38:16 +0100

    Change the icon color

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -1,30 +1,28 @@
-import { IconButton } from "./Buttons";
-import { WiMoonAltNew } from "react-icons/wi";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { IconButton2 } from './Buttons'
+import { WiMoonAltNew } from 'react-icons/wi'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 
 const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false)
+  const { resolvedTheme, setTheme } = useTheme()
 
-  useEffect(() => setMounted(true), []);
+  useEffect(() => setMounted(true), [])
   return (
     <motion.div
       whileTap={{ scale: 0.7, rotate: -30, elevation: 10 }}
       transition={{ duration: 0.3, damping: 10 }}
     >
       {mounted && (
-        <IconButton
+        <IconButton2
           aria="Theme Toggle"
-          onclick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-          icon={
-            <WiMoonAltNew className="text-3xl text-white dark:text-black" />
-          }
+          onclick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+          icon={<WiMoonAltNew className="text-3xl" />}
         />
       )}
     </motion.div>
-  );
-};
+  )
+}
 
-export default ThemeToggle;
+export default ThemeToggle
