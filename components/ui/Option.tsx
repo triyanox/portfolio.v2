b75de4a706078ae4ba14554879b1c9ab967d0047
Mohamed Achaq <motriyanox@gmail.com>
2022-07-21 16:37:22 +0100

    Change the text color

--- components/ui/Option.tsx
+++ components/ui/Option.tsx
@@ -1,50 +1,50 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from 'framer-motion'
 
 export type Option = {
-  text: string;
-  handleClick: () => void;
-};
+  text: string
+  handleClick: () => void
+}
 
 export const Option = (props: Option) => {
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.button
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transformOrigin: "left" }}
+        animate={{ opacity: 1, transformOrigin: 'left' }}
         exit={{ opacity: 0 }}
         transition={{
           duration: 0.8,
           stiffness: 100,
-          type: "spring",
+          type: 'spring',
           damping: 15,
         }}
         onClick={props.handleClick}
-        className="rounded-full text-2xl font-semibold  text-zinc-400 dark:text-zinc-600"
+        className="rounded-full text-2xl font-semibold  text-zinc-400 dark:text-zinc-700"
       >
         {props.text}
       </motion.button>
     </AnimatePresence>
-  );
-};
+  )
+}
 
 export const OptionSelected = (props: Option) => {
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.button
         initial={{ opacity: 0 }}
-        animate={{ opacity: 100, transformOrigin: "left" }}
+        animate={{ opacity: 100, transformOrigin: 'left' }}
         exit={{ opacity: 0 }}
         transition={{
           duration: 0.4,
           stiffness: 100,
-          type: "spring",
+          type: 'spring',
           damping: 15,
         }}
         onClick={props.handleClick}
-        className="rounded-full dark:text-white text-black text-2xl font-semibold "
+        className="rounded-full text-2xl font-semibold text-black dark:text-white "
       >
         {props.text}
       </motion.button>
     </AnimatePresence>
-  );
-};
+  )
+}
