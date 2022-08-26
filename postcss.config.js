e19a23bfd0238c42124c1cece5ed2c2700e53284
Mohamed Achaq <motriyanox@gmail.com>
2022-08-26 18:24:06 +0200

    Update the postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
-const tailwindcss = require('tailwindcss');
-const cssnano = require('cssnano')({
-  preset: 'default'
-})
-
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: cssnano } : {})
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
 }
