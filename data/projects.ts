95ec43b17ce50b08cd7580b6e93c083acdd4b9ef
Mohamed Achaq <contact@achaq.codes>
2023-01-11 13:08:28 +0100

    Update the projects list

--- data/projects.ts
+++ data/projects.ts
@@ -44,37 +44,6 @@ const projects = [
     preview: "https://plana.achaq.codes",
   },
   {
-    name: "Conversa",
-    description: "A WebSockets based text messaging app",
-    tech: [
-      Typescript,
-      Reactdotjs,
-      Nodedotjs,
-      Mongodb,
-      SocketIo,
-      Tailwindcss,
-      Express,
-    ],
-    link: "https://github.com/triyanox/conversa-client",
-    preview: "https://conversa.achaq.codes",
-  },
-  {
-    name: "Omoide",
-    description:
-      "A platform for sharing memories and experiences with the world",
-    tech: [
-      Typescript,
-      Reactdotjs,
-      Nextdotjs,
-      Nodedotjs,
-      Mongodb,
-      Tailwindcss,
-      Express,
-    ],
-    link: "https://github.com/triyanox/omoide-client",
-    preview: "https://omoide.achaq.codes",
-  },
-  {
     name: "rsmdc",
     description: "A markdown to HTML parser written in Rust",
     tech: [Rust],
@@ -103,21 +72,6 @@ const projects = [
     ],
     link: "https://github.com/triyanox/portfolio.v2",
     preview: "https://achaq.codes",
-  },
-  {
-    name: "Shortify",
-    description: "URL shortener platform",
-    tech: [
-      Typescript,
-      Reactdotjs,
-      Nextdotjs,
-      Nodedotjs,
-      Mongodb,
-      Tailwindcss,
-      Express,
-    ],
-    link: "https://github.com/triyanox/shortify-client",
-    preview: "https://shortify.achaq.codes",
   },
 ];
 
