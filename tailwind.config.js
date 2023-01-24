1e0fa567e8e507c90139dfa33a84f00fcfcf9ecc
Mohamed Achaq <contact@achaq.codes>
2023-01-24 14:29:50 +0200

    Remove tap highlight color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,76 +1,79 @@
 /** @type {import('tailwindcss').Config} */
-const { fontFamily } = require('tailwindcss/defaultTheme')
-const plugin = require('tailwindcss/plugin')
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: ['./pages/**/*.tsx', './components/ui/**/*.tsx', './layouts/**/*.tsx'],
-  darkMode: 'class',
+  content: [
+    "./pages/**/*.tsx",
+    "./components/ui/**/*.tsx",
+    "./layouts/**/*.tsx",
+  ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Wright Pro', ...fontFamily.sans],
+        sans: ["Wright Pro", ...fontFamily.sans],
       },
       animation: {
-        'gradient-x': 'gradient-x 3s ease infinite',
-        'gradient-y': 'gradient-y 3s ease infinite',
-        'gradient-xy': 'gradient-xy 3s ease infinite',
+        "gradient-x": "gradient-x 3s ease infinite",
+        "gradient-y": "gradient-y 3s ease infinite",
+        "gradient-xy": "gradient-xy 3s ease infinite",
       },
       keyframes: {
         loading: {
-          '0%': {
-            opacity: '.2',
+          "0%": {
+            opacity: ".2",
           },
-          '20%': {
-            opacity: '1',
-            transform: 'translateX(1px)',
+          "20%": {
+            opacity: "1",
+            transform: "translateX(1px)",
           },
           to: {
-            opacity: '.2',
+            opacity: ".2",
           },
         },
-        'gradient-y': {
-          '0%, 100%': {
-            'background-size': '400% 400%',
-            'background-position': 'center top',
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
           },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'center center',
-          },
-        },
-        'gradient-x': {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
           },
         },
-        'gradient-xy': {
-          '0%, 100%': {
-            'background-size': '400% 400%',
-            'background-position': 'left center',
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
         },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
     plugin(function ({ addUtilities }) {
       addUtilities({
-        'body::-webkit-scrollbar': {
-          display: 'none',
+        "body::-webkit-scrollbar": {
+          display: "none",
         },
-        'body::-webkit-tap-highlight-color' : 'transparent',
-        'button:focus': { outline: 'none' },
-      })
+        "button:focus": { outline: "none" },
+      });
     }),
   ],
-}
+};
