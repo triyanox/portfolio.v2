f80e3c1c6dcf78dca6d56c63e7edea3172e76db2
Mohamed Achaq <motriyanox@gmail.com>
2023-01-27 19:07:44 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <body className="relative scroll-smooth bg-white antialiased  dark:bg-black max-w-2xl">
+      <body className="relative scroll-smooth bg-white antialiased  dark:bg-black">
         <Main />
         <NextScript />
       </body>
